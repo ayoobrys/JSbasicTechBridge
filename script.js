22ec83426605652bf7bcb02a75f8d00d80be67e8
ayoobrys <jones.bryson428@gmail.com>
2022-03-15 18:30:43 -0400

    JSpractice2

--- script.js
+++ script.js
@@ -1,4 +1,21 @@
 let firstName = "Bryson";
 
-let message = "Hello my name is: " + firstName;
+let message = "Hello my name is: " + firstName + " and I like dogs";
+
 console.log(message);
+
+let firstName2 = "Allen"
+
+let age = 26;
+
+let message2 = "Hello my name is: " + firstName2 + "I enjoy long walks";
+
+console.log(message2);
+
+let myNumber = 9;
+
+let numberString = myNumber.toString()
+
+let message3 = "My number is " + numberString;
+
+console.log(message3);
